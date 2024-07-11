ba074011632d18274c804ce204729f6d99b2135b
arjun pangeni <eemc365@gmail.com>
2024-07-12 05:19:11 +0545

    code example

---
+++ oop_in_js/call_this.js
@@ -0,0 +1,15 @@
+function setUsername(username) {
+    this.username = username
+    console.log('called')
+}
+
+function createUser(username, email, password) {
+    setUsername.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+
+const chai = new createUser('arjun', 'arjun@gh', '123')
+console.log(chai)
+
